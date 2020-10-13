20d4bc559ffa5a0a2eb425ee7b697b305b11c05d
jancso-l <61355854+jancso-l@users.noreply.github.com>
2020-10-13 08:34:00 +0200

    Common completed

---
+++ Common/ZeroGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    class ZeroGenerator : INumberSequenceSource
+    {
+        public int N { get; set; }
+        public IEnumerable<int> GenerateNumbers()
+        {
+            for (int i = 0; i < N +1; i++)
+            {
+                yield return 0;
+            }
+            
+        }
+    };
+    {
+    }
+}
