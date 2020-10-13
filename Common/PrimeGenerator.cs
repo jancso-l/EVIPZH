c37773bc4b3fd3a6ac7d99c5fd256bdf718ef61a
jancso-l <61355854+jancso-l@users.noreply.github.com>
2020-10-13 09:41:40 +0200

    isPrime implemented

---
+++ Common/PrimeGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+    class PrimeGenerator : INumberSequenceSource
+    {
+        int N;
+        public PrimeGenerator(int N)
+        {
+            N = this.N;
+        }
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<int> GenerateNumbers()
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool isPrime(int x)
+        {
+            for (int i = 1; i <= x; i++)
+            {
+                if (x % i == 0)
+                {
+                    return false;
+                }                
+            }
+            return true;
+        }
+
+        
+    }
+}
