87b1b7bc7929fe3cf76314631bf97727c9d38c60
jancso-l <61355854+jancso-l@users.noreply.github.com>
2020-10-13 09:29:59 +0200

    Console progress

--- Common/INumberSequenceSource.cs
+++ Common/INumberSequenceSource.cs
@@ -7,6 +7,7 @@ namespace Common
     public interface INumberSequenceSource
     {
         IEnumerable<int> GenerateNumbers();
+        int Count();
     }
 
 
--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using Common;
+using L
 
 namespace ConsoleApplication
 {
@@ -6,7 +11,18 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ShowNumbers(5);
+        }
+        static List<int> ShowNumbers(INumberSequenceSource number)
+        {
+            int orderNumber = 0;
+            List<int> list = null;
+            int count = number.Count();
+            for (int i = 0; i < count; i++)
+            {
+                list.Add(number);
+            }
+            Console.WriteLine($"{orderNumber}. elem: {list.ElementAt()}");
         }
     }
 }
